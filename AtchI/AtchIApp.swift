841da067fb84b3303c17d6a801c986f79b31b7d7
leedoyeon849 <do83430208@gmail.com>
2023-03-09 13:09:42 +0900

    [Setting] Repository 설정

---
+++ AtchI/AtchIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtchIApp.swift
+//  AtchI
+//
+//  Created by DOYEON LEE on 2023/03/09.
+//
+
+import SwiftUI
+
+@main
+struct AtchIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
