165cdda7fcd78eb616f730c42bf10b7f2a43881f
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-03-24 15:43:43 +0900

    Button Size Update

---
+++ AtchI/Util/Constant/ButtonSize.swift
@@ -0,0 +1,16 @@
+//
+//  ButtonSize.swift
+//  AtchI
+//
+//  Created by 강민규 on 2023/03/24.
+//
+
+import Foundation
+
+enum ButtonSize {
+    static let largeWidth: CGFloat = 300
+    static let largeHeight: CGFloat = 30
+    
+    static let smallWidth: CGFloat = 85
+    static let smallHeight: CGFloat = 35
+}
