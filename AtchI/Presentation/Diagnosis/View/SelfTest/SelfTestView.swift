261d3b46130fb244d0ce962b26b06029d97c9009
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-03-27 02:43:00 +0900

    [FEAT] 프로그레스 뷰 패딩 조절

--- AtchI/Presentation/Diagnosis/View/SelfTest/SelfTestView.swift
+++ AtchI/Presentation/Diagnosis/View/SelfTest/SelfTestView.swift
@@ -30,6 +30,7 @@ struct SelfTestView: View {
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .padding(.horizontal, 30)
+                    .padding(.bottom, 4)
                 
                 // 자가진단 설명 뷰
                 Text("질문을 보고 10년 전과 비교해서 어떻게 변했는지 선택해주세요.")
@@ -53,59 +54,45 @@ struct SelfTestView: View {
                         ProgressView(value: viewModel.questionIndex, total: 14)
                             .progressViewStyle(LinearProgressViewStyle(tint: .mainPurple))
                             .animation(.default, value: viewModel.questionIndex)
-
                     }
                     .padding(.horizontal, 30)
-                    .padding(.bottom, 30)
-                    
+                    .padding(.bottom, 10)
                     
                     // 문제 View
                     //TODO: 카드뷰 패딩 넣어서 안움직이게 하기
                     SelfTestQuestionCardView(buttonSeletor: $buttonSeletor, id: $viewModel.questionIndex)
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 8)
+                        .padding(.horizontal, 25)
                         .animation(.easeIn, value: viewModel.questionIndex)
-                        
-                    
-                    Spacer()
                 }
                 
+                Spacer(minLength: 20)
                 
                 // 완료 버튼
-                HStack{
-                    // 왼쪽 공백
-                    Spacer()
-                    
-                    // 완료 버튼
-                    DefaultButton(
-                        buttonSize: .large,
-                        width: .infinity,
-                        buttonStyle: .filled,
-                        buttonColor: .mainPurple,
-                        isIndicate: false)
-                    {
-                        if buttonSeletor != nil {
-                            if viewModel.questionIndex == 14 {
-                                // TODO: 완료 결과 데이터 내보내기
-                                
-                            } else {
-                                viewModel.questionIndex += 1
-                                buttonSeletor = nil
-                            }
-                        }
-                    } content: {
+                DefaultButton(
+                    buttonSize: .large,
+                    buttonStyle: .filled,
+                    buttonColor: .mainPurple,
+                    isIndicate: false)
+                {
+                    if buttonSeletor != nil {
                         if viewModel.questionIndex == 14 {
-                            Text("완료하기")
+                            // TODO: 완료 결과 데이터 내보내기
+                            
                         } else {
-                            Text("다음으로")
+                            viewModel.questionIndex += 1
+                            buttonSeletor = nil
                         }
                     }
-                    .disabled(buttonSeletor == nil)
-                    .padding(.all, 30)
-                    
-                    // 오른쪽 공백
-                    Spacer()
+                } content: {
+                    if viewModel.questionIndex == 14 {
+                        Text("완료하기")
+                    } else {
+                        Text("다음으로")
+                    }
                 }
+                .disabled(buttonSeletor == nil)
+                .padding(.horizontal, 30)
+                .padding(.bottom, 30)
             }
         }
     }
@@ -119,96 +106,99 @@ struct SelfTestQuestionCardView: View {
     @Binding var buttonSeletor: TestAnswer?
     @Binding var id: Double
     var body: some View {
+        // 백그라운드
         VStack {
-            VStack {
-                // 문제
-                VStack(alignment: .leading){
-                    Text("\(SelfTestQuestions.questions[Int(id)].index)")
-                        .padding(.bottom, 7)
-                        .foregroundColor(.mainPurple)
-                        .fontWeight(.bold)
-                    HStack {
-                        Text("\(SelfTestQuestions.questions[Int(id)].content)")
-                        Spacer()
-                    }
-                }
-                
-                Spacer()
-                // 버튼 Set
-                // TODO: 버튼 액션 넣기
-                /// - Note:
-                ///     - 초기에 전부 light로 할까요?
-                ///     - 선택시 vivid 해지게?
-                ///     - 아무것도 안했을땐 연한색으로
-                ///     - 버튼 위치 수정
-                ///     - 패딩 디테일 고치기
-                ///     - 프로그레스 사용해보기
-                ///     - Navigation 타이틀, Text로 고치기
-                ///
-                VStack(spacing: 10) {
-                    DefaultButton(buttonSize: .small,
-                                  width: 300,
-                                  buttonStyle: .filled,
-                                  buttonColor: buttonSeletor == .never ? .mainPurple : .mainPurpleLight,
-                                  isIndicate: false)
-                    {
-                        if buttonSeletor != .never {
-                            buttonSeletor = .never
-                        } else {
-                            buttonSeletor = nil
-                        }
-                    } content: {
-                        Text("변화 없음")
-                    }
-                    
-                    DefaultButton(buttonSize: .small,
-                                  width: 300,
-                                  buttonStyle: .filled,
-                                  buttonColor: buttonSeletor == .little ? .mainPurple : .mainPurpleLight,
-                                  isIndicate: false)
-                    {
-                        if buttonSeletor != .little {
-                            buttonSeletor = .little
-                        } else {
-                            buttonSeletor = nil
-                        }
-                    } content: {
-                        Text("조금 나빠짐")
-                    }
-                    
-                    DefaultButton(buttonSize: .small,
-                                  width: 300,
-                                  buttonStyle: .filled,
-                                  buttonColor: buttonSeletor == .many ? .mainPurple : .mainPurpleLight,
-                                  isIndicate: false)
-                    {
-                        if buttonSeletor != .many {
-                            buttonSeletor = .many
-                        } else {
-                            buttonSeletor = nil
-                        }
-                    } content: {
-                        Text("많이 나빠짐")
-                    }
-                    
-                    DefaultButton(buttonSize: .small,
-                                  width: 300,
-                                  buttonStyle: .filled,
-                                  buttonColor: buttonSeletor == .nothing ? .mainPurple : .mainPurpleLight,
-                                  isIndicate: false)
-                    {
-                        if buttonSeletor != .nothing {
-                            buttonSeletor = .nothing
-                        } else {
-                            buttonSeletor = nil
-                        }
-                    } content: {
-                        Text("해당 없음")
-                    }
+            // 문제
+            VStack(alignment: .leading){
+                Text("\(SelfTestQuestions.questions[Int(id)].index)")
+                    .padding(.bottom, 7)
+                    .foregroundColor(.mainPurple)
+                    .fontWeight(.bold)
+                HStack {
+                    Text("\(SelfTestQuestions.questions[Int(id)].content)")
+                    Spacer()
                 }
             }
-            .padding(20)
+            
+            Spacer()
+            
+            // 버튼 Set
+            // TODO: 버튼 액션 넣기
+            /// - Note:
+            ///     - 초기에 전부 light로 할까요?
+            ///     - 선택시 vivid 해지게?
+            ///     - 아무것도 안했을땐 연한색으로
+            ///     - 버튼 위치 수정
+            ///     - 패딩 디테일 고치기
+            ///     - 프로그레스 사용해보기
+            ///     - Navigation 타이틀, Text로 고치기
+            ///
+            VStack(spacing: 10) {
+                DefaultButton(buttonSize: .small,
+                              width: 250,
+                              buttonStyle: .filled,
+                              buttonColor: buttonSeletor == .never ? .mainPurple : .mainPurpleLight,
+                              isIndicate: false)
+                {
+                    if buttonSeletor != .never {
+                        buttonSeletor = .never
+                    } else {
+                        buttonSeletor = nil
+                    }
+                } content: {
+                    Text("변화 없음")
+                }
+                
+                
+                DefaultButton(buttonSize: .small,
+                              width: 250,
+                              buttonStyle: .filled,
+                              buttonColor: buttonSeletor == .little ? .mainPurple : .mainPurpleLight,
+                              isIndicate: false)
+                {
+                    if buttonSeletor != .little {
+                        buttonSeletor = .little
+                    } else {
+                        buttonSeletor = nil
+                    }
+                } content: {
+                    Text("조금 나빠짐")
+                }
+
+                
+                
+                DefaultButton(buttonSize: .small,
+                              width: 250,
+                              buttonStyle: .filled,
+                              buttonColor: buttonSeletor == .many ? .mainPurple : .mainPurpleLight,
+                              isIndicate: false)
+                {
+                    if buttonSeletor != .many {
+                        buttonSeletor = .many
+                    } else {
+                        buttonSeletor = nil
+                    }
+                } content: {
+                    Text("많이 나빠짐")
+                }
+                
+                DefaultButton(buttonSize: .small,
+                              width: 250,
+                              buttonStyle: .filled,
+                              buttonColor: buttonSeletor == .nothing ? .mainPurple : .mainPurpleLight,
+                              isIndicate: false)
+                {
+                    if buttonSeletor != .nothing {
+                        buttonSeletor = .nothing
+                    } else {
+                        buttonSeletor = nil
+                    }
+                } content: {
+                    Text("해당 없음")
+                }
+            }
         }
+        .padding(25)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.grayBoldLine)
         .cornerRadius(20)
