30d378a7e01b674f3287cc79e28a8be351d632f7
Leebomyi <tak1203@naver.com>
2023-03-27 11:44:07 +0900

    [FEAT] 예방 탭 화면 구현 re-merge

---
+++ AtchI/Presentation/Prevent/View/Reusable/BoldDivider.swift
@@ -0,0 +1,18 @@
+//
+//  BoldDivider.swift
+//  AtchI
+//
+//  Created by 이봄이 on 2023/03/27.
+//
+
+import Foundation
+import SwiftUI
+
+struct BoldDivider: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color.grayBoldLine)
+            .frame(height: 15)
+            .edgesIgnoringSafeArea(.horizontal)
+    }
+}
