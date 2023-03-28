91f90304b81030ad3079381ea657afc93cf43143
leedoyeon849 <do83430208@gmail.com>
2023-03-29 03:04:10 +0900

    [Test] HealthKit 수면 데이터 Test 코드 작성

--- AtchI/AtchIApp.swift
+++ AtchI/AtchIApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct AtchIApp: App {
+    
+    let service = HealthKitService()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SleepTimeView()
         }
+    }
+    
+    init() {
+        setup()
+    }
+    
+    func setup() {
+        // 첫 실행 시 실행할 코드를 여기에 작성합니다.
+        service.configure()
     }
 }
