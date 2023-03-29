6de7f8dd82b5e1a69fca200bb69531d919c1be5d
Leebomyi <tak1203@naver.com>
2023-03-29 23:03:11 +0900

    [FEAT] 예방 탭 UI 수정

--- AtchI/Presentation/Prevent/View/PreventView.swift
+++ AtchI/Presentation/Prevent/View/PreventView.swift
@@ -9,11 +9,26 @@ import SwiftUI
 
 struct PreventView: View {
     var body: some View {
-        NavigationStack {
-            VStack {
+        VStack {
+            ScrollView {
+                HStack {
+                    Text("예방")
+                        .font(.titleLarge)
+                    Spacer()
+                    
+                    Text("퀴즈를 풀며 뇌를 훈련시켜 보세요!")
+                        .font(.bodyLarge)
+                        .frame(maxWidth: 250, maxHeight: 37)
+                        .background(Color.grayBoldLine)
+                        
+                        .cornerRadius(10)
+                        .padding(.vertical, 1)
+                }
+                .padding(EdgeInsets(top: 10, leading: 30, bottom: 0, trailing: 30))
+                
                 // 이번주 퀴즈 현황
                 QuizStateCard()
-                    .padding(EdgeInsets(top: 30, leading: 30, bottom: 0, trailing: 30))
+                    .padding(EdgeInsets(top: 10, leading: 30, bottom: 0, trailing: 30))
                 // 굵은 구분선
                 BoldDivider()
                     .padding(.top, 30)
@@ -36,18 +51,14 @@ struct PreventView: View {
                 
                 VStack {
                     QuizTemplate(quizOrder: "첫", quizCountents: "오늘은 몇월 며칠인가요?")
+                        .padding(.bottom, 20)
                     QuizTemplate(quizOrder: "두", quizCountents: "어제 누구랑 점심을 먹었나요?")
+                        .padding(.bottom, 20)
                     QuizTemplate(quizOrder: "세", quizCountents: "오늘 아침에 일어나자마자 무엇을 했나요?")
                 }
-                
                 .padding(.horizontal, 30)
                 Spacer()
             }
-            .listRowInsets(EdgeInsets())
-            .listRowSeparator(.hidden)
-
-            .navigationTitle("예방")
-            
         }
     }
 }
