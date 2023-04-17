d2e372e7f184b5d7e66db28862a1660015b47184
leedoyeon849 <do83430208@gmail.com>
2023-04-17 23:48:14 +0900

    [CHORE] 주석처리 코드 및 Doc주석 정리

--- AtchI/HealthKitTemp/HealthKitProvider.swift
+++ AtchI/HealthKitTemp/HealthKitProvider.swift
@@ -16,13 +16,6 @@ class HealthKitProvicer {
                                completion: @escaping ([HKCategorySample]) -> Void){
         // identifier로 Type 정의
         if let sleepType = HKObjectType.categoryType(forIdentifier: identifier) {
-            // 데이터를 필터링할 조건(predicate)를 설정할 수 있음. 여기선 일주일 데이터를 받아오도록 설정
-            /// ➡️ 여기서부터
-//            let calendar = Calendar.current
-//            let endDate = Date() // 현재 시간
-//            let startDate = calendar.date(byAdding: .day, value: -7, to: endDate) // 7일 전 시간
-//            let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate)
-            /// ➡️ 여기까지는 상위에서 주입
             // 최신 데이터를 먼저 가져오도록 sort 기준 정의
             let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
             // 쿼리 수행 완료시 실행할 콜백 정의
@@ -36,20 +29,6 @@ class HealthKitProvicer {
                     return
                 }
                 if let result = tmpResult {
-                    for item in result {
-                        if let sample = item as? HKCategorySample {
-                            // 가져온 데이터 출력
-                            print("Sleep value: \(sample.value)")
-                            print("Start Date: \(sample.startDate)")
-                            print("End Date: \(sample.endDate)")
-                            print("Metadata: \(String(describing: sample.metadata))")
-                            print("UUID: \(sample.uuid)")
-                            print("Source: \(sample.sourceRevision)")
-                            print("Device: \(String(describing: sample.device))")
-                            print("---------------------------------\n")
-                            
-                        }
-                    }
                     let categorySamples = result.map { $0 as! HKCategorySample }
                     completion(categorySamples)
                 }
