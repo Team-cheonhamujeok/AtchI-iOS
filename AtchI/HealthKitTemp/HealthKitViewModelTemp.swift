fe001bc2da7daa0d2e0e9745f827c73d3579bae1
leedoyeon849 <do83430208@gmail.com>
2023-04-25 23:23:23 +0900

    [FIX] 예시 HKViewModel 오류 해결
    - 여러 퍼블리셔들 중 오류 가능성이 있으므로 sink 할때 오류 처리 클로저를 써줘야함

--- AtchI/HealthKitTemp/HealthKitViewModelTemp.swift
+++ AtchI/HealthKitTemp/HealthKitViewModelTemp.swift
@@ -16,32 +16,31 @@ class HealthKitViewModelTemp {
     }
     
     /// 예시 함수
-//    func start() {
-//        let pub1 = sleepService.fetchSleepDataWithCombine(date: Date())
-//        let pub2 = sleepService.fetchSleepDataWithCombine(date: Date()) // 실제론 다른 서비스
-//        let pub3 = sleepService.fetchSleepDataWithCombine(date: Date()) // 실제론 다른 서비스
-//        
-//        // 실제로 동작이 멈추는건가? cancle -> complete. callback -> 자원을 아끼는가? ->>>
-//        
-//        // zip3 사용 예시
-//        pub1.zip(pub2, pub3) { [weak self] sleepSamples, samples2, samples3 in // 네이밍은 서비스에 맞게
-//            print("sample1: \(sleepSamples), sample2: \(samples2), sample3: \(samples3)") // 값 3개가 다 도착하면 실행
-//            
-//            let sleepEndDate = self?.sleepService.getSleepEndDate(samples: sleepSamples)
-//            let sleepCoreQuentity = self?.sleepService.getSleepCoreQuentity(samples: sleepSamples)
-//            // ... 쭉쭉 필요한 정보 계산
-//            
-//            
-//            
-//            // Life Pattern DTO(Model) 생성
-//            
-//            
-//            
-//            // 서버 저장 request 호출!
-//            
-//            
-//            
-//            
-//        }.sink {}
-//    }
+    func start() {
+        let pub1 = sleepService.fetchSleepDataWithCombine(date: Date())
+        let pub2 = sleepService.fetchSleepDataWithCombine(date: Date()) // 실제론 다른 서비스
+        let pub3 = sleepService.fetchSleepDataWithCombine(date: Date()) // 실제론 다른 서비스
+        
+        // 실제로 동작이 멈추는건가? cancle -> complete. callback -> 자원을 아끼는가? ->>>
+        
+        // zip3 사용 예시
+        pub1.zip(pub2, pub3) { [weak self] sleepSamples, samples2, samples3 in // 네이밍은 서비스에 맞게
+            print("sample1: \(sleepSamples), sample2: \(samples2), sample3: \(samples3)") // 값 3개가 다 도착하면 실행
+            
+            let sleepEndDate = self?.sleepService.getSleepEndDate(samples: sleepSamples)
+            let sleepCoreQuentity = self?.sleepService.getSleepCoreQuentity(samples: sleepSamples)
+            // ... 쭉쭉 필요한 정보 계산
+            
+            // Life Pattern DTO(Model) 생성
+            // ...
+            
+            // 서버 저장 request 호출!
+            // ...
+            
+        }.sink(receiveCompletion: { completion in
+            // completion 처리
+        }, receiveValue: { value in
+            // value 처리
+        })
+    }
 }
