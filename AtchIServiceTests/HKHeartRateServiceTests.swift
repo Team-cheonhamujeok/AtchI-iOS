680d24442440f633e439c6d0cfd60716c5f08068
Leebomyi <tak1203@naver.com>
2023-05-11 20:46:42 +0900

    [Feat] Get Day Sleep HeartRate & flatten

--- AtchIServiceTests/HKHeartRateServiceTests.swift
+++ AtchIServiceTests/HKHeartRateServiceTests.swift
@@ -15,13 +15,15 @@ final class HKHeartRateServiceTests: XCTestCase {
 
     var service: HKHeartRateService!
     var dateHelper: DateHelper!
+    var sleepService: HKSleepService!
     
-//    var cancellabe = Set<AnyCancellable>()
+    var cancellabe = Set<AnyCancellable>()
     
 
     override func setUpWithError() throws {
         self.service = HKHeartRateService(healthKitProvider: HKProvider())
         self.dateHelper = DateHelper()
+        self.sleepService = HKSleepService(provider: HKProvider(), dateHelper: DateHelper())
     }
 
     override func tearDownWithError() throws {
@@ -33,12 +35,44 @@ final class HKHeartRateServiceTests: XCTestCase {
         
         let expectation = XCTestExpectation(description: "Sleep HeartRate Test")
         
-        let cancellable = service.getSleepHeartRate(startDate: dateHelper.getYesterdayStartAM(Date()), endDate: dateHelper.getYesterdayEndPM(Date()))
-            .sink(receiveCompletion: { _ in print("### stream 완료") },
-                  receiveValue: { samples in
-                print("받아왔니? \(samples)")
-                expectation.fulfill()
-            })
+//        let cancellable = service.getSleepHeartRate(startDate: dateHelper.getYesterdayStartAM(Date()), endDate: dateHelper.getYesterdayEndPM(Date()))
+//            .sink(receiveCompletion: { _ in print("### stream 완료") },
+//                  receiveValue: { samples in
+//                print("받아왔니? \(samples)")
+//                expectation.fulfill()
+//            })
+        
+        let cancellable = sleepService.getSleepInterval(date: Date()).sink(receiveCompletion: { _ in print("@@@완료@@@")}, receiveValue: { interval in
+//            print("인터벌 내놔\(interval)")
+            
+            // get heartrate
+            let publishers = interval.map {
+                self.service.getSleepHeartRate(startDate: $0.startDate,
+                                          endDate: $0.endDate)
+                .eraseToAnyPublisher()
+            }
+            
+            Publishers.concatenateMany(publishers)
+                    .flatMap { $0.publisher }
+                    .collect()
+                    .sink(receiveCompletion: {_ in}, receiveValue: { value in
+                        print("Flattened value: \(value)")
+                        
+                        expectation.fulfill()
+                    }).store(in: &self.cancellabe)
+
+//            combinedPublisher
+//                .flatMap { $0.publisher }
+//                .collect()
+//                .sink { value in
+//                    print("Flattened value: \(value)")
+//                }
+            
+            
+            
+            
+
+        })
         
         wait(for: [expectation], timeout: 10.0)
 
@@ -52,4 +86,12 @@ final class HKHeartRateServiceTests: XCTestCase {
         }
     }
 
+}
+
+extension Publishers {
+    static func concatenateMany<Output, Failure>(_ publishers: [AnyPublisher<Output, Failure>]) -> AnyPublisher<Output, Failure> {
+        return publishers.reduce(Empty().eraseToAnyPublisher()) { acc, elem in
+            Publishers.Concatenate(prefix: acc, suffix: elem).eraseToAnyPublisher()
+        }
+    }
 }
