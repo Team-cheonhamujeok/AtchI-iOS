f6992e11b431afe1a9aa302bae0efe7765e14a4a
leedoyeon849 <do83430208@gmail.com>
2023-05-30 21:39:01 +0900

    [FIX] keyboard 변경 초 0.01로 변경, 살짝 애니메이션 보임

--- AtchI/Presentation/MMSE/View/Reusable/TextInputWithSuffix.swift
+++ AtchI/Presentation/MMSE/View/Reusable/TextInputWithSuffix.swift
@@ -53,7 +53,7 @@ struct TextInputWithSuffix: View {
                              DispatchQueue.main.async {
                                  if isFocused {
                                      isFocused = false
-                                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
                                          isFocused = true
                                      }
                                  }
