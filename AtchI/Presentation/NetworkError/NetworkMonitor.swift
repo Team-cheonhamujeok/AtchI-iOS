7e24fef98902ea32a0486d2b6e9611f3aed0475d
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-06-02 14:07:40 +0900

    [FEAT] 진단 세이프 에리아

--- AtchI/Presentation/NetworkError/NetworkMonitor.swift
+++ AtchI/Presentation/NetworkError/NetworkMonitor.swift
@@ -33,37 +33,36 @@ final class NetworkMonitor {
     }
     
     // Network Monitoring 시작
-        public func startMonitoring() {
-            monitor.start(queue: queue)
-            monitor.pathUpdateHandler = { [weak self] path in
+    public func startMonitoring() {
+        monitor.start(queue: queue)
+        monitor.pathUpdateHandler = { [weak self] path in
 
-                self?.isConnected = path.status == .satisfied
-                self?.getConnectionType(path)
+            self?.isConnected = path.status == .satisfied
+            self?.getConnectionType(path)
 
-                if self?.isConnected == true {
-                    print("연결됨!")
-                } else {
-                    print("연결안됨!")
-                }
-            }
-        }
-
-        // Network Monitoring 종료
-        public func stopMonitoring() {
-            monitor.cancel()
-        }
-
-        // Network 연결 타입
-        private func getConnectionType(_ path: NWPath) {
-            if path.usesInterfaceType(.wifi) {
-                connectionType = .wifi
-            } else if path.usesInterfaceType(.cellular) {
-                connectionType = .cellular
-            } else if path.usesInterfaceType(.wiredEthernet) {
-                connectionType = .ethernet
+            if self?.isConnected == true {
+                print("연결됨!")
             } else {
-                connectionType = .unknown
+                print("연결안됨!")
             }
         }
-    
+    }
+
+    // Network Monitoring 종료
+    public func stopMonitoring() {
+        monitor.cancel()
+    }
+
+    // Network 연결 타입
+    private func getConnectionType(_ path: NWPath) {
+        if path.usesInterfaceType(.wifi) {
+            connectionType = .wifi
+        } else if path.usesInterfaceType(.cellular) {
+            connectionType = .cellular
+        } else if path.usesInterfaceType(.wiredEthernet) {
+            connectionType = .ethernet
+        } else {
+            connectionType = .unknown
+        }
+    }
 }
