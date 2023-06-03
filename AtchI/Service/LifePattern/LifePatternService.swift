3567cb574287f4fc91613b1e5eec67efc6e44803
leedoyeon849 <do83430208@gmail.com>
2023-06-03 23:10:54 +0900

    [REFACT] LifePatternService 리팩토링
    - 내부 로직을 자잘한 sub function들로 변경하고 최상단 함수를 하나의 체이닝으로 묶음
    - TODO: 에러 핸들링

--- AtchI/Service/LifePattern/LifePatternService.swift
+++ AtchI/Service/LifePattern/LifePatternService.swift
@@ -10,10 +10,13 @@ import Foundation
 
 import Moya
 
+class LifePatternServiceType {
+    
+}
+
 class LifePatternService {
     
-    private var provider: MoyaProvider<LifePatternAPI> // FIXME: 주입으로 바꾸기
-    
+    private var provider: MoyaProvider<LifePatternAPI>
     private var sleepService: HKSleepServiceType
     private var activityService: HKActivityServiceProtocol
     private var heartRateService: HKHeartRateServiceType
@@ -38,40 +41,22 @@ class LifePatternService {
     /// 4. LifePatternModel들을 배열로 합쳐 서버에 저장합니다.
     ///
     /// - Returns: 요청 성공/실패에 대한 응답을 반환하는 퍼블리셔를 반환합니다.
-    func requestPostLifePattern() -> AnyPublisher<LifePatternResponseModel, Error> {
-        
+    func requestSaveLifePatterns() -> AnyPublisher<LifePatternResponseModel, LifePatternError> {
         let mid = UserDefaults.standard.integer(forKey: "mid")
         if mid <= 0 { fatalError("잘못된 접근입니다. 해당 API는 로그인된 상태에서 호출되어야합니다.") }
         
-        // 마지막 업데이트일 받아오기
-        let lastDatePublisher = reuqestLastDate(mid: mid)
-            .map { model in
-                if let lastModifiedDate = model.response.lastDate {
-                    return DateHelper.convertStringToDate(string: lastModifiedDate)
-                } else {
-                    return DateHelper.subtractDays(from: Date(), days: 120)
+        return requestLastDate(mid: mid)
+            .map { model -> Date in
+                return self.getlastDateForCreateLifePattern(lastDate: model.response.lastDate)
+            }
+            .map { lastDate -> [Date] in
+                return self.getDatesForCreateLifePatterns(lastDate: lastDate)
+            }
+            .map { dates -> [AnyPublisher<LifePatternModel, Never>] in
+                return dates.compactMap { date -> AnyPublisher<LifePatternModel, Never> in
+                    return self.createLifePatternModel(date: date)
                 }
             }
-
-        // 마지막 업데이트일과 오늘 사이의 모든 날짜에 대한 LifePatternModel구하기
-        //  -> AnyPublisher<LifePatternModel, Never>
-        let lifePatternPublisher = lastDatePublisher
-            .replaceError(with: Date())
-            .map { lastDate -> [AnyPublisher<LifePatternModel, Never>]  in
-                // 사이의 모든 날짜 구하기
-                let dates = DateHelper.generateBetweenDates(from: lastDate, to: Date())
-
-                // Calendar와 DateComponents를 사용하여 100일 전(임시)의 날짜 계산
-                return dates.compactMap { date -> AnyPublisher<LifePatternModel, Never>? in
-                        return self.createLifePatternModel(date: date)
-                    }
-            }
-            .replaceError(with: nil)
-            .compactMap { $0 }
-            .eraseToAnyPublisher()
-        
-        // LifePattern모델들을 배열로 합쳐서 서버에 푸시하기
-        return lifePatternPublisher
             .flatMap {
                 Publishers.MergeMany($0)
             }
@@ -86,13 +71,21 @@ class LifePatternService {
                     }
                     .eraseToAnyPublisher()
             }
+            .mapError { _ in
+                LifePatternError.sendFailed
+            }
             .eraseToAnyPublisher()
+        
     }
     
-    func reuqestLastDate(mid: Int) -> AnyPublisher<ResponseModel<LastDateResponseModel>, Error> {
+    
+    /// mid에 해당하는 유저의 LifePattern 마지막 업데이트 일을 가져옵니다.
+    /// - Parameter mid: 로그인된 유저 mid입니다.
+    /// - Returns: 마지막 업데이트 날짜를 담은 모델을 반환합니다.
+    func requestLastDate(mid: Int) -> AnyPublisher<ResponseModel<LastDateResponseModel>, Error> {
         
         // 마지막 업데이트일 받아오기
-       return provider.requestPublisher(.lastDate(mid))
+        return provider.requestPublisher(.lastDate(mid))
             .tryMap { response in
                 return try response.map(ResponseModel<LastDateResponseModel>.self)
             }
@@ -104,6 +97,29 @@ class LifePatternService {
 }
 
 extension LifePatternService {
+    // MARK: - Private functions
+    
+    /// LifePattern을 생성할 범위의 마지막 날짜 (오래전 날짜)를 구합니다.
+    ///
+    /// - Note: 만약 첫 업데이트(lastDate가 없다면)라면 120일 전을 마지막 날짜로 정합니다.
+    /// - Parameter lastDate: 마지막 날짜를 전달받습니다. 없다면 nil을 받습니다.
+    /// - Returns: 마지막 날짜를 Date형으로 반환합니다.
+    private func getlastDateForCreateLifePattern(lastDate: String?) -> Date {
+        if let lastModifiedDate = lastDate {
+            return DateHelper.convertStringToDate(string: lastModifiedDate)
+        } else {
+            return DateHelper.subtractDays(from: Date(), days: 120)
+        }
+    }
+    
+    /// LifePattern을 생성할 범위에 해당하는 Date 배열을 생성합니다.
+    /// - Parameter lastDate: 범위의 마지막 날짜를 받습니다.
+    /// - Returns: Date 배열을 반환합니다.
+    /// - Note: 범위의 시작 날짜는 오늘입니다.
+    private func getDatesForCreateLifePatterns(lastDate: Date) -> [Date] {
+        return DateHelper.generateBetweenDates(from: lastDate, to: Date())
+    }
+    
     /// HKService조합해서 여러 값을 LifePatternModel로 묶습니다.
     private func createLifePatternModel(date: Date) -> AnyPublisher<LifePatternModel, Never> {
         let stepCountPub = activityService.getStepCount(date: date)
